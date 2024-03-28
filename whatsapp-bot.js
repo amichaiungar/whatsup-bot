eb68f35f3b3c03a4faf3ba2810b627bc797e8e66
amichaiu <amichai.ungar@nice.com>
2024-03-28 11:05:39 +0200

    small fix

--- whatsapp-bot.js
+++ whatsapp-bot.js
@@ -1,18 +1,12 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client } = require('whatsapp-web.js');
 const qrcode = require("qrcode-terminal");
 
 const {findMeInAllTabs, updateCacheWithSpreadSheet, whoIsNow, whoIsLater, searchSoldierAtListByPhone, convertFromIndexToTimeAndPlaceInWhatsupFormat} = require("./googleSheetsService");
 
-const constants = require('./constants');
-
 let answerNum = -1;
 const answers = ["חחחח לפחות עד יולי", "מי שלא צם לא משתחרר...", "בפנסיה", "כששפיר ישמור לילות", "כשמוזס יתחיל לזרוק", "הרבנות תדאג לנו ל4 המינים?"];
 //const client = new Client({ session: sessionData });
-const client = new Client({
-    authStrategy: new LocalAuth({
-        clientId: "client-one"
-    })
-});
+const client = new Client();
 
 // Event: When the client is ready
 client.on('ready', async() => {
@@ -23,7 +17,7 @@ client.on('ready', async() => {
 function getAnswerForWhenAreWeGettingOut() {
     answerNum ++;
     answerNum = answerNum % answers.length;
-     return answers[answerNum];
+    return answers[answerNum];
 }
 
 // Event: When a new message arrives
@@ -31,47 +25,49 @@ client.on('message', async (msg) => {
     try {
         const chat = await msg.getChat();
         let number = chat.id.user;
-        let valueToFind = searchSoldierAtListByPhone(number);
-        if (valueToFind == null) {
-            console.log("number of unlisted is:" + number);
-            await msg.reply("נו נו נו. המספר שלך לא מוכר במערכת. עדכן את הקובץ ולאחר מכן עדכן את עמיחי שבוצע");
-        } else {
-            valueToFind = valueToFind.name;
-            let func = msg.body.trim();
-            if (func === 'מתי' || func === '1' || func === 'אני') {
-                let response = await findMeInAllTabs({
-                    valueToFind
-                })
-                //console.log('output for getSpreadSheetTabs', JSON.stringify(response), null, 2);
-                console.log('output for convertFromIndexToTimeAndPlaceInWhatsupFormat ' + valueToFind + ": ", JSON.stringify(convertFromIndexToTimeAndPlaceInWhatsupFormat(response), null, 2));
-                await msg.reply(convertFromIndexToTimeAndPlaceInWhatsupFormat(response));
-            } else if (func === '?') {
-                let text = "אופציות לבוט (שלח הודעה עם המלל המודגש):" + "\r\n" +
-                    " המילה *מתי* או *אני* או *1*: מראה לך מתי אתה שומר" + "\r\n" +
-                    "*עכשיו* או *2*: מי שומר עכשיו?" + "\r\n" +
-                    "*הבא* או *3*: מי במשמרת הבאה?" + "\r\n" +
-                    "*עד מתי* או *4*: מתי משתחררים?";
-                await msg.reply(text);
-            } else if (func === 'עכשיו' || func === '2') {
-                let text = await whoIsNow();
-                await msg.reply(text);
-            } else if (func === 'הבא' || func === '3') {
-                let text = await whoIsLater()
-                await msg.reply(text);
-            } else if (func === 'עד מתי' || func === '4' || func === 'עד מתי?') {
-                let text;
-                if  (valueToFind === 'רועי לוי')
-                    text= getAnswerForWhenAreWeGettingOut();
-                await msg.reply(text);
-            } else if (func === "007") {
-                console.log("reloading sheet");
-                await updateCacheWithSpreadSheet();
+            let valueToFind = searchSoldierAtListByPhone(number);
+            if (valueToFind == null) {
+                console.log("number of unlisted is:" + number);
+                await msg.reply("נו נו נו. המספר שלך לא מוכר במערכת. עדכן את הקובץ ולאחר מכן עדכן את עמיחי שבוצע");
             } else {
-                console.log(valueToFind + " :" + func + " אופציה לא חוקית");
-                await msg.reply(func + " היא אופציה לא חוקית" + "\r\n" +
-                    "הקש *?* בשביל תפריט");
+                valueToFind = valueToFind.name;
+                let func = msg.body.trim();
+                if (func === 'מתי' || func === '1' || func === 'אני') {
+                    let response = await findMeInAllTabs({
+                        valueToFind
+                    })
+                    //console.log('output for getSpreadSheetTabs', JSON.stringify(response), null, 2);
+                    console.log('output for convertFromIndexToTimeAndPlaceInWhatsupFormat ' + valueToFind + ": ", JSON.stringify(convertFromIndexToTimeAndPlaceInWhatsupFormat(response), null, 2));
+                    await msg.reply(convertFromIndexToTimeAndPlaceInWhatsupFormat(response));
+                } else if (func === '?') {
+                    let text = "אופציות לבוט (שלח הודעה עם המלל המודגש):" + "\r\n" +
+                        " המילה *מתי* או *אני* או *1*: מראה לך מתי אתה שומר" + "\r\n" +
+                        "*עכשיו* או *2*: מי שומר עכשיו?" + "\r\n" +
+                        "*הבא* או *3*: מי במשמרת הבאה?" + "\r\n" +
+                        "*עד מתי* או *4*: מתי משתחררים?";
+                    await msg.reply(text);
+                } else if (func === 'עכשיו' || func === '2') {
+                    let text = await whoIsNow();
+                    await msg.reply(text);
+                } else if (func === 'הבא' || func === '3') {
+                    let text = await whoIsLater()
+                    await msg.reply(text);
+                } else if (func === 'עד מתי' || func === '4' || func === 'עד מתי?') {
+                    let text;
+                    if  (valueToFind === 'רועי לוי'){
+                        text = "1.4.2024";
+                    } else
+                        text= getAnswerForWhenAreWeGettingOut();
+                    await msg.reply(text);
+                } else if (func === "007") {
+                    console.log("reloading sheet");
+                    await updateCacheWithSpreadSheet();
+                } else {
+                    console.log(valueToFind + " :" + func + " אופציה לא חוקית");
+                    await msg.reply(func + " היא אופציה לא חוקית" + "\r\n" +
+                        "הקש *?* בשביל תפריט");
+                }
             }
-        }
     }catch (e){
         console.error(e);
     }
